866af4e7533d62089d8dcbae57baddf39144e246
qwillie64 <kuan.wei.05@gmail.com>
2023-06-05 02:13:17 +0800

    Finish the music search function

--- Main_Test/WebScutter.py
+++ Main_Test/WebScutter.py
@@ -1,9 +1,10 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+from random import randint
+from lxml import etree
 
 
 chrome_options = Options()
@@ -11,35 +12,69 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--log-level=1")
 
 
-
-def search_google(keywords:str) : 
+# Search music from spotify
+def search_google(keywords: str):
     driver = webdriver.Chrome(options=chrome_options)
     driver.get("https://www.google.com.tw/")
 
     search_box = driver.find_element(By.NAME, "q")
     search_box.send_keys(keywords)
     search_box.send_keys(Keys.RETURN)
-    results = driver.find_elements(By.CLASS_NAME, 'c7r50')
-    time.sleep(2)
+    results = driver.find_elements(By.CLASS_NAME, "c7r50")
+    time.sleep(randint(2, 5))
 
     for i in results:
         print(i.text)
 
     driver.quit()
-    
 
-def search_spotify(keywords:str) : 
+
+# Search music from spotify
+def search_spotify(keywords: str, random: bool = False, max: int = 3) -> dict:
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get(f"https://open.spotify.com/search/{keywords.replace(' ','%20')}/playlists")
+    driver.get(
+        f"https://open.spotify.com/search/{keywords.replace(' ','%20')}/playlists"
+    )
     time.sleep(2)
-    results = driver.find_elements(by=By.CLASS_NAME, value='Nqa6Cw3RkDMV8QnYreTr')
-    time.sleep(2)
+    results = driver.find_elements(By.CLASS_NAME, "Nqa6Cw3RkDMV8QnYreTr")
+    time.sleep(randint(1, 3))
 
-    driver.quit()
+    playlists = []
     for i in results:
-        print(i.get_attribute('href'))
+        playlists.append(i.get_attribute("href"))
+    driver.quit()
 
-    
-    
+    count = 0
+    songs = {}
+    for list in playlists:
+        # print(f"url = {list}")
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get(list)
+        time.sleep(2)
+        song_name = driver.find_elements(
+            By.XPATH,
+            "//*[@class='Type__TypeElement-sc-goli3j-0 cvuJgi t_yrXoUO3qGsJS4Y6iXX standalone-ellipsis-one-line']",
+        )
+        time.sleep(2)
+        song_artist = driver.find_elements(
+            By.XPATH,
+            "//*[@class='Type__TypeElement-sc-goli3j-0 fjvaLo rq2VQ5mb9SDAFWbBIUIn standalone-ellipsis-one-line']",
+        )
+        time.sleep(randint(1, 3))
 
-search_spotify("anime song")
+        for i in range(0, len(song_name)):
+            songs[song_name[i].text] = song_artist[i].text
+        driver.quit()
+
+        if random:
+            count += randint(1, 5)
+        else:
+            count += 1
+
+        if count >= max:
+            break
+        time.sleep(randint(1, 3))
+    return songs
+
+
+print(search_spotify("anime song", False, 10))
